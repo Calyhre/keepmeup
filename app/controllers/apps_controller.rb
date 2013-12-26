8f9c09f1c146e1708daeaa48fe3845d08959e591
Calyhre <calyhre@gmail.com>
2013-12-26 16:29:49 +0100

    Stuff AppsController

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,4 +1,17 @@
 class AppsController < ApplicationController
   def index
+    @apps = App.order(:name).decorate
+  end
+
+  def update
+    @app = App.find params[:id]
+    @app.update_attributes app_params
+    redirect_to root_path anchor: @app.name
+  end
+
+  private
+
+  def app_params
+    params.require(:app).permit(:ping_disabled, :maintenance)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Keepmeup::Application.routes.draw do
+  resources :apps, only: [:update]
+
   root 'apps#index'
 end

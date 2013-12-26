c6dde523d403c3e191cb1937e8e91228ac060549
Calyhre <calyhre@gmail.com>
2013-12-26 20:02:00 +0100

    Add cache on partial

--- app/models/heroku_process.rb
+++ app/models/heroku_process.rb
@@ -1,5 +1,5 @@
 class HerokuProcess < ActiveRecord::Base
-  belongs_to :app, primary_key: 'name', foreign_key: 'app_name'
+  belongs_to :app, primary_key: 'name', foreign_key: 'app_name', touch: true
 
   scope :dynos, -> { where("process LIKE 'web.%'") }
   scope :ups, -> { where("state = 'up'") }
